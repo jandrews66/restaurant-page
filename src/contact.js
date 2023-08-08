2bdbad8defec2bc71da0d8d9027048bc06ab3e7e
Joe Andrews <joeandrews92@gmail.com>
2023-08-08 14:10:14 -0700

    Add content to menu and contact pages

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,15 @@
 const contactModule = () => {
     const headline = document.createElement("h3");
-    headline.textContent = "Contact page";
+    headline.textContent = "Contact";
     content.appendChild(headline);
+
+    const phone = document.createElement("div")
+    phone.innerHTML = "Telephone: +1 (604) 838292"
+    content.appendChild(phone);
+
+    const hours = document.createElement("div")
+    hours.innerHTML = "Monday: Closed <br>Tuesday: 11am - 9pm <br>Wednesday: 11am - 9pm <br>Thursday: 11am - 9pm <br>Friday: 11am - 10pm <br>Saturday: 11am - 10pm <br>Sunday: 10:30am - 8pm";
+    content.appendChild(hours);
 }
 
 export default contactModule;
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,19 @@
 const menuModule = () => {
     const headline = document.createElement("h3");
-    headline.textContent = "Menu page";
+    headline.textContent = "Menu";
     content.appendChild(headline);
+
+    const createMenuItem = (name, description, price) => {
+        const dish = document.createElement("div");
+        dish.innerHTML = name + '<br>' + description + '<br>' + price;
+        content.appendChild(dish)
+    }
+    
+    const steakFrites = createMenuItem("Steak Frites", "6oz sirloin served with crispy french fries and a bernaise sauce", "$25")
+    const coqAuVin = createMenuItem("Coq au Vin", "An old Coq braised in red wine, lardon and wild mushrooms served with a side of pomme puree", "$22")
+    const nicoiseSalad = createMenuItem("Nicoise Salad", "A salad of tuna, new potoatoes, green beans, soft boiled egg with a lemon mustard vinegrette", "$21")
 }
+
+
 
 export default menuModule;
